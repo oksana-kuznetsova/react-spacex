503614ae60d7bef35411d582e6e3ccc3a6f7290e
oksana-kuznetsova <riskpisk@yandex.ru>
2020-10-14 17:36:52 +0300

    main

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
+import Features from './components/Features/Features'
+import Details from './components/Details/Details'
+import Calendar from './components/Calendar/Calendar'
+
 
 import './style.css';
 
@@ -11,6 +15,9 @@ function App() {
     <>
       <Header />
       <Main />
+      <Features/>
+      <Details/>
+      <Calendar/>
       <Footer/>
     </>
   );
